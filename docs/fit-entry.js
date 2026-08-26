c9cda2daf9e24f1e1d0df2a4b9d8250dfab4f630
edsonjunioor32 <edsonjunioor32@gmail.com>
2026-08-26 12:30:37 -0300

    Inicializa ramo de histórico do pipeline

--- docs/fit-entry.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  'use strict';
-  // Recurso de aderência temporariamente oculto do portal público.
-  // Mantemos o arquivo para facilitar a reativação após a correção do analisador.
-})();
